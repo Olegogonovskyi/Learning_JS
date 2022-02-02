8082a84fa483977de5a79ad40bd364fac40cb83d
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-02-02 18:26:47 +0200

    Js Less1 homework in process

--- Leson1/Claswork/main.js
+++ Leson1/Claswork/main.js
@@ -1,64 +1,68 @@
-// // 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor).
-// // Скласти результат цих чисел в змінній result.
-// let a = Math.floor(Math.random()*100);
-// let b = Math.floor(Math.random()*100);
-// let c = Math.floor(Math.random()*100);
-// let d = Math.floor(Math.random()*100);
-// let e = Math.floor(Math.random()*100);
-// let f = Math.floor(Math.random()*100);
-// let j = Math.floor(Math.random()*100);
-// let k = Math.floor(Math.random()*100);
-// let l = Math.floor(Math.random()*100);
-// let m = Math.floor(Math.random()*100);
-//
-// let arr = new Array(a,b,c,d,e,f,j,k,l,m)
-// let result = (a+b+c+d+e+f+j+k+l+m)
-// console.log(arr)
-// console.log(result)
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor).
+// Скласти результат цих чисел в змінній result.
+let a = Math.floor(Math.random()*100);
+let b = Math.floor(Math.random()*100);
+let c = Math.floor(Math.random()*100);
+let d = Math.floor(Math.random()*100);
+let e = Math.floor(Math.random()*100);
+let f = Math.floor(Math.random()*100);
+let j = Math.floor(Math.random()*100);
+let k = Math.floor(Math.random()*100);
+let l = Math.floor(Math.random()*100);
+let m = Math.floor(Math.random()*100);
 
-// // 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
-// // 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
-// // 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. ' +
-// // 'Вивести кожну книгу як окремий об'єкт
-//
-// let book1 = {
-//     title: 'Bandera',
-//     pages: 365,
-//     gen: 'history',
-//
-// }
-// let book2 = {
-//     title: 'Red point',
-//     pages: 24,
-//     gen: 'Genealogy',
-//     autor: 'Losenko',
-// }
-//
-//     books = new Array(book1,book2)
-//     console.log(books)
-// console.log(book1)
-// console.log(book2)
+let arr = new Array(a,b,c,d,e,f,j,k,l,m)
+let result = (a+b+c+d+e+f+j+k+l+m)
+console.log(arr)
+console.log(result)
 
-// // 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height)
-// // та шириною 10см (змінна width). Значення площі зберігати в змінній s.
-//
-// let height = 23;
-// let width = 10;
-// let s = height*width
-// console.log(s+ 'см2')
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+// 4. Створити маис об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. ' +
+// 'Вивести кожну книгу як окремий об'єкт
 
-// // 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
-// // результат помістіть у змінну v.
-//
-// let heightC = 10;
-// let dC = 4;
-// const Pi =3.1415;
-//
-// let v = Pi*(dC/2)**2*heightC;
-// console.log(v)
+let book1 = {
+    title: 'Bandera',
+    pages: 365,
+    gen: 'history',
+
+}
+let book2 = {
+    title: 'Red point',
+    pages: 24,
+    gen: 'Genealogy',
+    autor: 'Losenko',
+}
+
+    books = new Array(book1,book2)
+    console.log(books)
+console.log(book1)
+console.log(book2)
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height)
+// та шириною 10см (змінна width). Значення площі зберігати в змінній s.
+
+let height = 23;
+let width = 10;
+let s = height*width
+console.log(s+ 'см2')
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+
+let heightC = 10;
+let dC = 4;
+const Pi =3.1415;
+
+let v = Pi*(dC/2)**2*heightC;
+console.log(v)
 // У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
 // Знайдіть гіпотенузу k за теоремою Піфагора
 // (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
 
-let n = 3;
-let m = 4;
+{
+    let n = 3;
+    let m = 4;
+    let c = Math.sqrt(Math.pow(n,2) + Math.pow(m,2))
+    console.log(c)
+}
