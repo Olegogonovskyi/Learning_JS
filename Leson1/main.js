37f2b2a8c9be875812c93660916af01f24011934
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-02-02 09:22:50 +0200

    Js Less1 homework in process

---
+++ Leson1/main.js
@@ -0,0 +1,213 @@
+
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.writelet hel ='hello';
+{let xxx = hel
+console.log(xxx);
+alert(xxx);
+document.write(xxx);}
+
+let o ='owu';
+{let xxx = o
+console.log(xxx);
+alert(xxx);
+document.write(xxx);}
+
+let c ='com';
+{
+    let xxx = c
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+let u ='ua';
+{
+    let xxx = u
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+let n1 = 1;
+{
+    let xxx = n1
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+let n10 =10;
+{
+    let xxx = n10
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+let nnn =-999;
+{
+    let xxx = nnn
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+let nn =123;
+{
+    let xxx = nn
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+let pi =3.14;
+{
+    let xxx = pi
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+let notpi =2.7;
+{
+    let xxx = notpi
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+let ven =16;
+{
+    let xxx = ven
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+let rig =true;
+{
+    let xxx = rig
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+let nrig =false;
+{
+    let xxx = nrig
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+// Переприсвоїти кожній змінній з завдання значення на довільне.
+//     Вивести кожну змінну за допомогою: console.log , alert, document.write
+hel ='bye';
+{let xxx = hel
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);}
+
+o ='oun';
+{let xxx = o
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);}
+
+c ='net';
+{
+    let xxx = c
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+u ='uk';
+{
+    let xxx = u
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+n1 = 2;
+{
+    let xxx = n1
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+n10 =20;
+{
+    let xxx = n10
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+nnn =-333;
+{
+    let xxx = nnn
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+nn =321;
+{
+    let xxx = nn
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+pi =4.13;
+{
+    let xxx = pi
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+notpi =7.2;
+{
+    let xxx = notpi
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+ven =30;
+{
+    let xxx = ven
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+rig =false;
+{
+    let xxx = rig
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+nrig =true;
+{
+    let xxx = nrig
+    console.log(xxx);
+    alert(xxx);
+    document.write(xxx);
+}
+
+
+
+
+
+
+
+
+
+
