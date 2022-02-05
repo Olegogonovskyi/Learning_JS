918c08f09f4cee73307d56f1156cdae4fb5714ad
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-02-05 23:08:05 +0200

    js less3 homework inproces

---
+++ Lesson3/classwork/main.js
@@ -0,0 +1,49 @@
+// зробити масив з 10 чисел [2,17,13,6,22,31,45,66,100,-18]та:
+let arr=[2,17,13,6,22,31,45,66,100,-18];
+// // перебрати його циклом while
+// let i = 0;
+// while (i<=arr.length) {
+//     console.log(arr[i])
+//     i++
+// }
+// // перебрати його циклом for
+
+// for (let number of arr) {
+//     console.log(number)
+// }
+// перебрати циклом while та вивести  числа тільки з непарним індексом
+// let i = 0;
+// while (i<=arr.length) {
+//     if (i % 2 !== 0) {
+//     console.log(arr[i]) }
+//     i++
+// }
+// // перебрати циклом for та вивести  числа тільки з непарним індексом
+// for (i=0; i<=arr.length; i++) {
+//     if (i % 2 !== 0) {
+//     console.log(arr[i])
+//     }
+// }
+// // перебрати циклом while та вивести  числа тільки парні  значення
+//
+// let i=0;
+// while (i<=arr.length) {
+//     if (arr[i] % 2 === 0) {
+//         console.log(arr[i])
+//     }
+//     i++
+// }
+// // перебрати циклом for та вивести  числа тільки парні  значення
+// for (let number of arr) {
+//     if (number % 2 ===0) {
+//         console.log(number)
+//     }
+// }
+// // замінити кожне число кратне 3 на слово "okten"
+//
+// for (i=0; i<=arr.length; i++) {
+//     if (arr[i] % 3 === 0) {
+//     arr[i] = 'okten'
+//     }
+// }
+// console.log(arr)
