eb72e6c30db6073f69876051e4471e756c00a0a6
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-02-08 07:12:45 +0200

    js less4 homm in proces

---
+++ Lesson4/Homework/main.js
@@ -0,0 +1,19 @@
+// // - створити функцію яка обчислює та повертає площу
+// // прямокутника зі сторонами а і б
+//
+// function PentArea (a,b) {
+//     return a*b;
+// }
+// console.log(PentArea(10,20));
+
+// // - створити функцію яка обчислює та повертає площу кола з радіусом r
+// function circleArea (r) {
+//     return Math.PI*Math.pow(r,2);
+// }
+// console.log(circleArea(10))
+
+// // - створити функцію яка обчислює та повертає площу циліндру висотою h, та радіутом r
+// function cylinderArea (h,r) {
+//     return 2*Math.PI*Math.pow(r,2) + 2*Math.PI*r*h;
+// }
+// console.log(cylinderArea(10,5));
