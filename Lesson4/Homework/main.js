43543d6e0b7043a0a9cf1e3fe4e6bcead6ddfd0b
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-02-08 08:25:29 +0200

    js less4 homm in proces

--- Lesson4/Homework/main.js
+++ Lesson4/Homework/main.js
@@ -17,3 +17,72 @@
 //     return 2*Math.PI*Math.pow(r,2) + 2*Math.PI*r*h;
 // }
 // console.log(cylinderArea(10,5));
+
+// // створити функцію яка приймає масив та виводить кожен його елемент
+// function arrLog () {
+//     for (let i = 0; i < arguments.length; i++) {
+//         const argument = arguments[i];
+//         console.log(argument)
+//     }
+// }
+// arrLog(30, 12, 'dddd', true)
+
+// // - створити функцію яка створює параграф з текстом. Текст задати через аргумент
+// function textWrite (txt) {
+//     document.write(`<p> ${txt} </p>`)
+// }
+// textWrite('Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusantium, aperiam architecto consectetur consequatur delectus deleniti dolor dolorem dolores dolorum expedita ipsum laborum libero magni, molestias nesciunt numquam, porro possimus quis quos reiciendis rem sequi tempore vel veniam vero. Magnam, perspiciatis.')
+
+// // - створити функцію яка створює ul з трьома елементами li. Текст li задати через аргумент всім однаковий
+// function createUlLi(item) {
+//     document.write(`<ul>`)
+//     document.write(`<li> ${item} </li>`)
+//     document.write(`<li> ${item} </li>`)
+//     document.write(`<li> ${item} </li>`)
+//     document.write(`</ul>`)
+// }
+// createUlLi('armstrong');
+//
+// // - створити функцію яка створює ul з трьома елементами li.
+// //     Текст li задати через аргумент всім однаковий.
+// //     Кількість li визначається другим аргументом, який є числовим (тут використовувати цикл)
+// function createUlLiCount (count, item) {
+//     document.write(`<ul>`);
+//     for (let i = 0; i < count; i++) {
+//        document.write(`<li> ${item} </li>`)
+//     }
+//     document.write(`</ul>`)
+// }
+// createUlLiCount(10, 'Lorem ipsum dolor.');
+
+// // - створити функцію яка приймає масив примітивних елементів (числа,стрінги,булеві), та будує для них список
+// function arrList() {
+//         document.write(`<ul>`);
+//     for (let i = 0; i < arguments.length; i++) {
+//         const argument = arguments[i];
+//         document.write(`<li> ${argument} </li>`)
+//     }
+//     document.write(`</ul>`)
+// }
+// arrList(1,34,-67, true, 'lorem', false);
+
+// - створити функцію яка приймає масив
+// об'єктів з наступними полями id,name,age , ' +
+// 'та виводить їх в документ. Для кожного об'єкту окремий блок.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
