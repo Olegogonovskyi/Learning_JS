90b6cf766c75dfd9c099ff678e9bfc37eb8e702b
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-02-13 17:00:21 +0200

    js less4 class done (not all)

--- Lesson 6/homework/main.js
+++ Lesson 6/homework/main.js
@@ -1,8 +1,3 @@
-//
-// // - Перевести до нижнього регістру настипні стрінгові значення
-// // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
-// // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
-// //
 // // - Перевести до великого регістру наступні стрінгові значення
 // // 'hello world', 'lorem ipsum', 'javascript is cool'
 //
@@ -23,8 +18,50 @@
 // console.log(somestring1.toUpperCase());
 // console.log(somestring2.toUpperCase());
 
-// - Перевести до нижнього регістру настипні стрінгові значення
-// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+// // - Перевести до нижнього регістру настипні стрінгові значення
+// // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+//
+// let somestring3 = 'hello world';
+// console.log(somestring3.toLowerCase());
+// let somestring4 = 'LOREM IPSUM';
+// console.log(somestring4.toLowerCase());
+// let somestring5 = 'JAVASCRIPT IS COOL';
+// console.log(somestring5.toLowerCase());
 
-let somestring3 = 'hello world';
-console.log(somestring3.toLowerCase());
+// // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+// let dertystr = ' dirty string   ';
+// console.log(dertystr.trim());
+
+// // - Напишіть функцію stringToarray(str), яка перетворює рядок на масив слів.
+// //     let str = 'Каждый охотник желает знать';
+// // let arr = stringToarray(str);
+// // document.writeln(arr); // ['Каждый', 'охотник', 'желает', 'знать']
+// let arr =  (str) => {
+//     return str.split(' ');
+// }
+// let str = 'Каждый охотник желает знать';
+//     document.write(arr(str));
+//
+// - Напишіть функцію delete_characters(str, length),
+//     яка повертає підрядок, що складається із зазначеної кількості символів.
+//     let str = 'Каждый охотник желает знать';
+// document.writeln(delete_characters(str, 7)); // Каждый
+
+let delete_characters = (str, length) =>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
