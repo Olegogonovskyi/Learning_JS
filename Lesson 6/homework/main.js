df286fcceffb3f2973963ca8635cfbacc8a435cb
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-02-13 16:08:23 +0200

    js less4 class done (not all)

---
+++ Lesson 6/homework/main.js
@@ -0,0 +1,30 @@
+//
+// // - Перевести до нижнього регістру настипні стрінгові значення
+// // 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+// // - Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+// //
+// // - Перевести до великого регістру наступні стрінгові значення
+// // 'hello world', 'lorem ipsum', 'javascript is cool'
+//
+// let somestring0 = 'hello world'
+// let strimgLong = (strng) => strng.length;
+// console.log(strimgLong(somestring0));
+//
+// let somestring1 = 'lorem ipsum'
+// document.write(strimgLong(somestring1));
+//
+// let somestring2 = 'avascript is cool'
+// alert(strimgLong(somestring2));
+
+// // - Перевести до великого регістру наступні стрінгові значення
+// // 'hello world', 'lorem ipsum', 'javascript is cool'
+//
+// console.log(somestring0.toUpperCase());
+// console.log(somestring1.toUpperCase());
+// console.log(somestring2.toUpperCase());
+
+// - Перевести до нижнього регістру настипні стрінгові значення
+// 'HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+let somestring3 = 'hello world';
+console.log(somestring3.toLowerCase());
