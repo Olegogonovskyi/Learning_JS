17dccac5cb60919ed26984e1f2cff86baddc231d
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-03-19 09:44:19 +0200

    Js Less8 homework done, classwork not all

---
+++ Lesson 13/main13.js
@@ -0,0 +1,25 @@
+setTimeout(() =>{
+    let morningwork = (`6:00 дзвонить будильник, переставляю його на 10 хв`)
+    console.log(morningwork)
+    setTimeout(() =>{
+        let morningwork = (`6:10 дзвонить будильник, злий як чорт встаю і йду на кухню`)
+        console.log(morningwork)
+        setTimeout(() =>{
+            let morningwork = (`6:20 мию грішне тіло`)
+            console.log(morningwork)
+            setTimeout(()=>{
+                let morningwork = (`6:30 кава+ сніданок`)
+                console.log(morningwork)
+                setTimeout(()=>{
+                    let morningwork = (`6:40 читаю новини і офігіваю`)
+                    console.log(morningwork)
+                    setTimeout(()=>{
+                        let morningwork = (`6:50 вибігаю з хати`)
+                        console.log(morningwork)
+                    },3000)
+                },3000)
+            },3000)
+        }, 3000)
+        }, 3000)
+    }, 3000
+)
