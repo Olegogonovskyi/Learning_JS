8ec5d1963cb18609b3fb314739404eadf685e054
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-03-20 13:57:58 +0200

    Js Less8 homework done, classwork not all

--- MiniProject/user_details_js.js
+++ MiniProject/user_details_js.js
@@ -3,4 +3,9 @@ let id =    JSON.parse(url.searchParams.get('id'))
 
 fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
     .then((response) => response.json())
-    .then(jsonResponses )
+    .then(jsonResponses => {
+        userdetails = document.createElement('div')
+        userdetails.classList.add ('userdetails')
+        userdetails.innerText = jsonResponses.id
+
+    })
