35e8e2f5fb1b0ee0c72615a462964814116f2440
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-03-20 13:30:43 +0200

    Js Less8 homework done, classwork not all

--- MiniProject/usersjs.js
+++ MiniProject/usersjs.js
@@ -1,14 +1,18 @@
+// let wrap = document.createElement('div')
+// wrap.classList.add('wrap')
+// document.body.appendChild(wrap)
+let wrap = document.getElementsByClassName('wrap')[0];
+
 fetch('https://jsonplaceholder.typicode.com/users')
-.then((response) => response.json()).then(jsonResponses => {
-    let wrap = document.createElement('div')
-wrap.classList.add('wrap')
-        document.body.appendChild(wrap)
+.then((response) => response.json())
+    .then(jsonResponses => {
+
     for (const user of jsonResponses) {
-        let user = document.createElement('div')
-        user.classList.add('user')
-        wrap.appendChild(user)
-
-            }
-}
-
-)
+        let userbox = document.createElement('div')
+        userbox.classList.add('userbox')
+        let anch = document.createElement('a')
+        anch.innerText = `${user.id} - ${user.name}`
+        anch.href = `./user_details.html`
+        userbox.appendChild(anch)
+        wrap.appendChild(userbox)
+    }});
