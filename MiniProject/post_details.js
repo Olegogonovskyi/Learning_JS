1dc8dc584ae8b34f8f281c03ce049df46b4283d8
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-03-21 16:11:11 +0200

    js less11 homm is done

--- MiniProject/post_details.js
+++ MiniProject/post_details.js
@@ -4,13 +4,23 @@ let id = url.searchParams.get('id')
 console.log(id)
 
 
-fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
+fetch(`https://jsonplaceholder.typicode.com/posts/${id}`)
     .then((response) => response.json())
     .then(jsonResponses => {
         let post = document.getElementsByClassName('post')[0]
         let postdetail = JSON.stringify(jsonResponses)
     post.innerHTML = postdetail
+        fetch(`https://jsonplaceholder.typicode.com/posts/${id}/comments`)
+            .then((response) => response.json())
+            .then(jsonResponses => {
+                for (const comment of jsonResponses) {
+                    let commentblock = document.createElement('div')
+                    commentblock.classList.add('commentblock')
+                    commentblock.innerText = comment.body
+                    post.appendChild(commentblock)
+                }
 
+            })
 
 
 
