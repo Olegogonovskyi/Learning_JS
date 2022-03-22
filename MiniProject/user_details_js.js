31ffa7301c6b1409542dcc15b504faf77ab0f31e
Olegogonovskyi <Olegogonowskyi@gmail.com>
2022-03-22 06:35:27 +0200

    js project not done

--- MiniProject/user_details_js.js
+++ MiniProject/user_details_js.js
@@ -12,32 +12,26 @@ fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
         let userdetails = document.createElement('div')
         userdetails.classList.add ('userdetails')
         userdetails.innerHTML = aboutuser
-        // let anch = document.createElement("a")
-        // anch.href = `./post-details.html?id=${id}`
-        //     anch.innerText = `Всі пости ${jsonResponses.name}`
-        // userdetails.appendChild(anch)
-        wrap.appendChild(userdetails)
-
-    })
-
-let button = document.createElement('button')
-button.innerText = `Всі пости`
-wrap.appendChild(button)
-button.onclick = function () {
+                wrap.appendChild(userdetails)
+        let button = document.createElement('button')
+        button.innerText = `Всі пости`
+        wrap.appendChild(button)
+        button.onclick = function () {
+                       this.disabled = true
             fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
-            .then((response) => response.json())
-            .then(jsonResponses => {
+                .then((response) => response.json())
+                .then(jsonResponses => {
                     for (const postitem of jsonResponses) {
-                            let forposts = document.getElementsByClassName(`forposts`)[0]
-                            let post = document.createElement('div')
-                            post.classList.add ('post')
-                            post.innerText = `Назва посту: ${postitem.title}`
-                            let postbutton = document.createElement('button')
-                            postbutton.innerText = 'Коментарі до посту'
-                            postbutton.onclick = function () {
-                                    location.href = `./post-details.html?id=${postitem.id}`
-                            }
-                            forposts.append(post,postbutton)
+                        let forposts = document.getElementsByClassName(`forposts`)[0]
+                        let post = document.createElement('div')
+                        post.classList.add ('post')
+                        post.innerText = `Назва посту: ${postitem.title}`
+                        let postbutton = document.createElement('button')
+                        postbutton.innerText = 'Коментарі до посту'
+                        postbutton.onclick = function () {
+                            location.href = `./post-details.html?id=${postitem.id}`
+                        }
+                        forposts.append(post,postbutton)
 
 
 
@@ -45,8 +39,39 @@ button.onclick = function () {
 
 
 
-            })
+                })
 
-}
+        }
+
+    })
+
+// let button = document.createElement('button')
+// button.innerText = `Всі пости`
+// wrap.appendChild(button)
+// button.onclick = function () {
+//             fetch(`https://jsonplaceholder.typicode.com/users/${id}/posts`)
+//             .then((response) => response.json())
+//             .then(jsonResponses => {
+//                     for (const postitem of jsonResponses) {
+//                             let forposts = document.getElementsByClassName(`forposts`)[0]
+//                             let post = document.createElement('div')
+//                             post.classList.add ('post')
+//                             post.innerText = `Назва посту: ${postitem.title}`
+//                             let postbutton = document.createElement('button')
+//                             postbutton.innerText = 'Коментарі до посту'
+//                             postbutton.onclick = function () {
+//                                     location.href = `./post-details.html?id=${postitem.id}`
+//                             }
+//                             forposts.append(post,postbutton)
+//
+//
+//
+//                     }
+//
+//
+//
+//             })
+//
+// }
 
 
